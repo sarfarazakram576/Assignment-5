be4c4b47ccf4302e3152a4f53780898e22de8029
sarfarazakram576 <sarfaraz.akram055@gmail.com>
2025-03-01 16:27:30 +0600

    Add first three box and activity log and connect blog.html with second box of first three box

--- events.js
+++ events.js
@@ -1,6 +1,19 @@
 // navbar theme change event 
 document.getElementById('theme-change-img').addEventListener('click', function(){
     let randomColor = '#' + Math.floor(Math.random()*1000000).toString();
-    document.body.style.backgroundColor = randomColor;
+    document.getElementById('html').style.backgroundColor = randomColor;
 })
 
+// current date
+const newDate = new Date();
+const currentDate = newDate.toDateString().split(' ');
+const dayName = currentDate[0];
+document.getElementById('day-name').innerText = dayName + ',';
+const showDate = currentDate[1] + ' ' + currentDate[2] + ' ' +  currentDate[3]
+document.getElementById('current-date').innerText = showDate;
+
+
+// go to blog.html
+document.getElementById('goToBlog').addEventListener('click', function(){
+    window.location.href = 'blog.html'
+})
